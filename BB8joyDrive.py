b765d7d4de5aae71679a6ae23b18c1038fe6ff8e
Christopher Lindsay <42160806+clindsa1@users.noreply.github.com>
2018-12-17 10:55:48 -0500

    updated BB8joyDrive.py
    Modified buttons to better match my Logitech F310 gamepad.
    Do not need coordinates or grid.
    QUIT now joins the Bluetooth thread before exit(), which should ensure the disconnect before killing the process. This seems to eliminate issue where BB8 was not connecting when restarting process.

--- BB8joyDrive.py
+++ BB8joyDrive.py
@@ -1,30 +1,27 @@
-import pygame
+import pygame, math, struct
 from pygame.locals import *
 from sys import exit
 from bluepy import btle
-import math
-import struct
 import BB8_driver
 
+# Startup BB8 stuff66
 bb8 = BB8_driver.Sphero()
 bb8.connect()
-
+bb8.start()
+#bb8.set_rgb_led(255, 0, 128, 0, False)
+bb8.set_rotation_rate(1, False)
+bb8.get_version(True)
 
 # Initializes Pygame & sets screen size (Width x Height)
 pygame.init()
 screen = pygame.display.set_mode((256, 275), 0, 32)
 pygame.display.set_caption("BB8 Drive")
-
-pygame.joystick.init()
-pygame.joystick.Joystick(0).init()
-
-
-bb8.set_rgb_led(0, 0, 0, 0, False)
-
-bb8.set_rotation_rate(1, False)
-
 clock = pygame.time.Clock()
 
+# Joystick stuff
+pygame.joystick.init()
+pygame.joystick.Joystick(0).init()
+joystick = pygame.joystick.Joystick(0)
 
 def clamp(value, minValue, maxValue):
     if value > maxValue:
@@ -47,18 +44,17 @@ def sendRollCommand(speed, heading):
         bb8.roll(0, lastHeading, 0, False)
 
 def draw_axis(surface, x, y, axis_x, axis_y, size):
-    line_col = (128, 128, 128)
+    #line_col = (128, 128, 128)
     num_lines = 40
     step = size / float(num_lines)
     joystickCenterX = 128
     joystickCenterY = 128
-    
 
     # Draws grid
-    for n in range(num_lines):
-        line_col = [(64, 64, 64), (89, 89, 89)][n & 1]
-        pygame.draw.line(surface, line_col, (x + n * step, y), (x + n * step, y + size))
-        pygame.draw.line(surface, line_col, (x, y + n * step), (x + size, y + n * step))
+#    for n in range(num_lines):
+#        line_col = [(64, 64, 64), (89, 89, 89)][n & 1]
+#        pygame.draw.line(surface, line_col, (x + n * step, y), (x + n * step, y + size))
+#        pygame.draw.line(surface, line_col, (x, y + n * step), (x + size, y + n * step))
 
     pygame.draw.line(surface, (200, 200, 200), (x, y + size / 2), (x + size, y + size / 2))
     pygame.draw.line(surface, (200, 200, 200), (x + size / 2, y), (x + size / 2, y + size))
@@ -97,26 +93,33 @@ def draw_axis(surface, x, y, axis_x, axis_y, size):
     heading = int(joystickAngleDegrees)
 
     sendRollCommand(speed, heading)
-
     
-    if joystick.get_button(6):
+    if joystick.get_button(6): #Back
         bb8.roll(0, heading, 0, False)
 
-    if joystick.get_button(4) == 1:
+    if joystick.get_button(4) == 1: #LB
         bb8.set_heading(0, False)
 
-    if joystick.get_button(5) == 1:
+    if joystick.get_button(5) == 1: #RB
         bb8.set_back_led(255, False)
-
     if joystick.get_button(5) == 0:
         bb8.set_back_led(0, False)
 
+    # Set BB8 Color based on joystick button color
+    if joystick.get_button(0) == 1: #A
+        bb8.set_rgb_led(0, 255, 0, 0, True)
+    if joystick.get_button(1) == 1: #B
+        bb8.set_rgb_led(255, 0, 0, 0, False)
+    if joystick.get_button(2) == 1: #X
+        bb8.set_rgb_led(0, 0, 255, 0, False)
+    if joystick.get_button(3) == 1: #Y
+        bb8.set_rgb_led(255, 128, 0, 0, True)
 
     # Displays the joystick X & Y coordinates to screen
-    message = "X: {}  Y: {} Speed: {} Heading: {}".format(draw_x, draw_y, speed, heading)
-    font = pygame.font.SysFont("arial", 15);
-    text_surface = font.render(message, True, (255, 153, 0))
-    screen.blit(text_surface, (1, 260))
+#    message = "X: {}  Y: {} Speed: {} Heading: {}".format(draw_x, draw_y, speed, heading)
+#    font = pygame.font.SysFont("arial", 15);
+#    text_surface = font.render(message, True, (255, 153, 0))
+#    screen.blit(text_surface, (1, 260))
 
     # Calculates joystick indicator and vector line
     draw_pos = (draw_x, draw_y)
@@ -126,37 +129,28 @@ def draw_axis(surface, x, y, axis_x, axis_y, size):
     pygame.draw.line(surface, (114, 153, 0), center_pos, draw_pos, 2)
     pygame.draw.circle(surface, (134, 179, 0), draw_pos, 10)
 
-
 while True:
     clock.tick(10)
 
-    joystick = pygame.joystick.Joystick(0)
-
-    for event in pygame.event.get():
+    for event in pygame.event.get(): #exit
         if event.type == QUIT:
+            print "Exiting"
+            bb8.disconnect()
+            bb8.join() #this should make sure that the disconnect is performed
             pygame.quit()
             exit()
-
-    if event.type == KEYDOWN:
-        if event.key >= K_0 and event.key <= K_1:
-            num = event.key - K_0
-
-
-    # axis_size = min(256, 640 / (joystick.get_numaxes()/2))
-    axis_size = min(256, 256)
-
-    #    pygame.draw.rect(screen, (255, 255, 255), (0, 0, 256, 275))
+        if event.type == KEYDOWN: #keyboard nubmers
+            if event.key >= K_0 and event.key <= K_1:
+                num = event.key - K_0
+                print num
+    
+    #this clears the screen
     pygame.draw.rect(screen, (38, 38, 38), (0, 0, 256, 275))
 
     # Draw all the axes (analog sticks)
-    x = 0
-    axis_x = joystick.get_axis(0)
-    axis_y = joystick.get_axis(1)
+    axis_x = joystick.get_axis(3)
+    axis_y = joystick.get_axis(4)
 
-
-
-    draw_axis(screen, x, 0, axis_x, axis_y, axis_size)
-    x += axis_size
-    
+    draw_axis(screen, 0, 0, axis_x, axis_y, 256)
 
     pygame.display.update()
